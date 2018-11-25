5c1d2681fe636b586f9271f0b988b32979d848f6
dmunozan <dmunozan@users.noreply.github.com>
2018-11-25 10:41:25 +0200

    Create mutation.js

---
+++ Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/mutation.js
@@ -0,0 +1,37 @@
+/*
+ * Basic Algorithm Scripting: Mutations
+ * 
+ * Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+ * 
+ * For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+ * 
+ * The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+ * 
+ * Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+ * 
+ *** Conditions ***
+ *
+ * mutation(["hello", "hey"]) should return false.
+ * mutation(["hello", "Hello"]) should return true.
+ * mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) should return true.
+ * mutation(["Mary", "Army"]) should return true.
+ * mutation(["Mary", "Aarmy"]) should return true.
+ * mutation(["Alien", "line"]) should return true.
+ * mutation(["floor", "for"]) should return true.
+ * mutation(["hello", "neo"]) should return false.
+ * mutation(["voodoo", "no"]) should return false.
+ */
+
+function mutation(arr) {
+  // Convert strings to lower case.
+  let str1 = arr[0].toLowerCase();
+  let str2 = arr[1].toLowerCase();
+
+  // Loop through the second string returning false if it finds a character not appearing in the first string.
+  for (let i = 0; i < str2.length; i++) {
+    if (str1.indexOf(str2.charAt(i)) < 0) {
+      return false;
+    }
+  }
+  return true;
+}

82bb863fd3f681891d0b30742c361fc2192fa395
dmunozan <dmunozan@users.noreply.github.com>
2018-12-09 11:13:59 +0200

    Create pairElement.js

---
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/pairElement.js
@@ -0,0 +1,44 @@
+/*
+ * Intermediate Algorithm Scripting: DNA Pairing
+ * 
+ * The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+ * 
+ * Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+ * 
+ * Return the provided character as the first element in each array.
+ * 
+ * For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+ * 
+ * The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+ * 
+ *** Conditions ***
+ * pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+ * pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+ * pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
+ */ 
+
+function pairElement(str) {
+  // Convert str to array for proper handling
+  var strArray = str.split("");
+
+  // Loop through the strArray adding the base pair to strand
+  var strand = [];
+  for (var i = 0; i < strArray.length; i++) {
+    switch (strArray[i]) {
+      case 'A':
+        strand.push(["A","T"]);
+        break;
+      case 'T':
+        strand.push(["T","A"]);
+        break;
+      case 'C':
+        strand.push(["C","G"]);
+        break;
+      case 'G':
+        strand.push(["G","C"]);
+        break;
+    }
+  }
+
+  return strand;
+}
